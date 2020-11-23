f7d4e6cb7688a367db601fc1259fca96c956ff6c
Catherine Chen <catherinehzl.chen@gmail.com>
2020-11-23 14:58:02 +0100

    solved

---
+++ tests/tool_test.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+
+# Import from standard library
+import os
+import mlproject
+import pandas as pd
+
+# Import from our lib
+from mlproject.tools import haversine
+import pytest
+
+
+def test_clean_data():
+    assert haversine(48.865070, 2.380009, 48.235070, 2.393409) == 70.00789153218594
